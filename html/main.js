2516ca7abecf8fe003ec79fa383dcbede3cc7455
Miche <mkuhlburger@gmail.com>
2017-11-14 19:53:02 +0100

    added an initScene function in main.js

--- html/main.js
+++ html/main.js
@@ -1,35 +1,42 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.shadowMapEnabled = true;
-document.body.appendChild( renderer.domElement );
-
-var cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-var cubeMaterial = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
-var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
-cube.castShadow = true;
-scene.add( cube );
 
 
-var planeGeometry = new THREE.PlaneGeometry(50, 50, 1, 1);
-var planeMaterial = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
-var plane = new THREE.Mesh( planeGeometry, planeMaterial );
+var scene,camera,renderer,cube,plane,spotLight;
 
-plane.rotation.x = -0.4 * Math.PI;
-plane.position.z = -5;
-plane.receiveShadow = true; 
-scene.add( plane );
+function initScene(){
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-camera.position.z = 5;
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	document.body.appendChild( renderer.domElement );
+
+	var cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var cubeMaterial = new THREE.MeshLambertMaterial( { color: 0x0000ff } );
+	cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+	cube.castShadow = true;
+	scene.add( cube );
 
 
-var spotLight = new THREE.SpotLight( 0xffffff );
-spotLight.position.set( -50, 50, 50 );
-spotLight.castShadow = true;
-scene.add( spotLight ); 
+	var planeGeometry = new THREE.PlaneGeometry(50, 50, 1, 1);
+	var planeMaterial = new THREE.MeshLambertMaterial( { color: 0xff0000 } );
+	plane = new THREE.Mesh( planeGeometry, planeMaterial );
 
+	plane.rotation.x = -0.4 * Math.PI;
+	plane.position.z = -5;
+	plane.receiveShadow = true; 
+	scene.add( plane );
+
+	camera.position.z = 5;
+	
+	
+	spotLight = new THREE.SpotLight( 0xffffff );
+	spotLight.position.set( -50, 50, 50 );
+	spotLight.castShadow = true;
+	scene.add( spotLight ); 
+}
+
+initScene();
 
 var animate = function () {
 	requestAnimationFrame( animate );
