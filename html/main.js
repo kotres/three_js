f259ec52873a74bdd19a0eeb92b49567c7afcea5
Michel Kuhlburger <michel.kuhlburger@cpe.fr>
2017-11-15 10:37:18 +0100

    changed camera position

--- html/main.js
+++ html/main.js
@@ -4,7 +4,7 @@ var scene,camera,renderer,cube,plane,spotLight;
 
 function initScene(){
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -27,7 +27,9 @@ function initScene(){
 	plane.receiveShadow = true; 
 	scene.add( plane );
 
-	camera.position.z = 5;
+    camera.position.z = 5;
+    camera.position.y = 5;
+    camera.rotation.x = -Math.PI/4;
 	
 	
 	spotLight = new THREE.SpotLight( 0xffffff );
